15e0cfc6bb963948fc201f3a80bf686052e65c46
Ndriçim Lahu <ndriqim.lahu@cacttus.education>
2021-01-14 00:33:42 +0100

    Added function findUserByEmailAndPassword in util.php

--- task-management/util.php
+++ task-management/util.php
@@ -4,4 +4,25 @@
     function isUserLoggedIn() {
         return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
     }
+
+    function findUserByEmailAndPassword($email, $password) {
+        global $dbConnection;
+
+        $sqlQuery = "SELECT * FROM user WHERE email=:email
+                         AND password=:password";
+
+        $encryptedPassword = md5($password);
+        
+        $statement = $dbConnection->prepare($sqlQuery);
+        $statement->bindParam(':email', $email);
+        $statement->bindParam(':password', $encryptedPassword);
+
+        if ($statement->execute()) {
+           $user = $statement->fetch(PDO::FETCH_ASSOC);
+           if($user !== false){
+                return $user;
+           }
+        }
+        return null;
+    }
 ?>
