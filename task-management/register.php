f79614b0fd71ca1b37ee3982ca8f8ef573007c6d
Ndriçim Lahu <ndriqim.lahu@cacttus.education>
2021-01-14 11:21:39 +0100

    Added the register form

---
+++ task-management/register.php
@@ -0,0 +1,33 @@
+<?php
+    session_start();
+    require_once "util.php";
+
+    // if logged in then redirect to tasklist.php
+    if (isUserLoggedIn()) {
+        header("Location: /task-management/tasklist.php");
+        die();
+    }
+?>
+<html>
+<head>
+    <title>Task Management Tool | Register</title>
+</head>
+<body>
+    <center>
+        <br><br>
+        <p><b>Task Management Tool - REGISTER</b></p>
+        <br><br>
+        <form method="POST" action="/task-management/register_api.php">
+            <label>Full Name:</label><br>
+            <input type="text" name="full_name"/><br><br>
+            <label>Email:</label><br>
+            <input type="email" name="email"/><br><br>
+            <label>Password:</label><br>
+            <input type="password" name="password"/><br><br>
+            <input type="submit" value="Register"/>
+        </form>
+        <br>
+        <a href="/task-management/login.php">Login</a> if you already have an account!
+    </center>
+</body>
+</html>
