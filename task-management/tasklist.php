ebbeb5594e1a9c2374ebca1f1ef86fdb0e07e020
Ndriçim Lahu <ndriqim.lahu@cacttus.education>
2021-01-15 16:04:40 +0100

    Added box for task title, description, status dropdown, button delete and styled with CSS

--- task-management/tasklist.php
+++ task-management/tasklist.php
@@ -12,15 +12,52 @@
 <head>
     <title>Task Management Tool | TASKLIST</title>
 </head>
+<style>
+#task_content {
+  width: 50%;
+  background-color: lightgrey;
+  border: 1px solid black;
+  margin: 20px;
+  padding: 20px;
+  text-align: justify;
+}
+#task_status {
+    float: right;
+    margin: 10px;
+    padding: 5px;
+}
+
+#task_delete {
+    background-color: red;
+    color: white;
+    float: right;
+    margin: 10px;
+    padding: 5px;
+}
+</style>
 <body>
     <center>
         <br><br>
         <p><b>Task Management Tool - Tasklist</b></p>
         <br><br>
-        <button id="task_add" type="button"><a href="/cacttus-s3-basic-web/task-management/taskadd_api.php">Add Task</a></button>
+        <button id="task_add" type="button"><a href="/cacttus-s3-basic-web/task-management/taskadd.php">Add Task</a></button>
         |
         <button id="sign_out" type="button"><a href="/cacttus-s3-basic-web/task-management/signout.php">Sign out</a></button>
-        <br>
+        <br><br>
+        <div id="task_content">
+            <button id="task_delete" type="button">DELETE</button>
+            <select id="task_status">
+                <option>ToDo</option>
+                <option>InProgress</option>
+                <option>Done</option>
+            </select>
+            <div id="task_title">
+                1. The title of the task
+            </div>
+            <div id="task_description">
+                The description the task, The description the task, The description the task, The description the task, <br> The description the task...
+            </div>
+        </div>
     </center>
 </body>
 </html>
