9ca1bdb91383fd254e64ab8f702c95dcf175beed
Ndriçim Lahu <ndriqim.lahu@cacttus.education>
2021-01-18 16:52:42 +0100

    Added the logic for delete task in the file taskdelete_api.php

--- task-management/taskdelete_api.php
+++ task-management/taskdelete_api.php
@@ -1,3 +1,10 @@
 <?php
-    // code
+    require_once "util.php";
+
+    if(isset($_GET['delete'])){
+        $id_task = $_GET['delete'];
+        deleteTaskFromDatabase($id_task);      
+    };
+
+    header("Location: /cacttus-s3-basic-web/task-management/tasklist.php");
 ?>
