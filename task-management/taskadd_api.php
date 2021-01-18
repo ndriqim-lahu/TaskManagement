ce6f2526abe987b6e871daaf2e83ad95dca7ac3d
Ndriçim Lahu <ndriqim.lahu@cacttus.education>
2021-01-18 16:43:05 +0100

    Added the logic for add task in the file taskadd_api.php

--- task-management/taskadd_api.php
+++ task-management/taskadd_api.php
@@ -1,3 +1,22 @@
 <?php
-    // code
+    session_start();
+    require_once "util.php";
+	
+    // get the data
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $status = $_POST['status'];
+    $id = $_SESSION['id'];
+
+    // fill the data
+    $task = [
+        'title' => $title,
+        'description' => $description,
+        'status' => $status,
+        'id' => $id
+    ];
+
+    storeTaskToDatabase($task);
+
+    header("Location: /cacttus-s3-basic-web/task-management/tasklist.php");
 ?>
